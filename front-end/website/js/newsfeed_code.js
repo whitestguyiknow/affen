5d799b23317d58d44ad18d895b26df8284f72784
IgorSusmelj <igor_7779@hotmail.com>
2015-03-21 13:30:30 +0100

    updated css and fixed error in newsfeed code

--- front-end/website/js/newsfeed_code.js
+++ front-end/website/js/newsfeed_code.js
@@ -5,8 +5,10 @@ $(document).ready(function(){
 function updateTicker(){
 	//$(".newsfeed_block").find("p").eq(0).remove();
 	if($(".newsfeed_block").find("p").length > 5)
-		$(".newsfeed_block").find("p").eq(0).remove();
+		$(".newsfeed_block").find("p").eq(0).slideUp('slow', function(){
+			$(this).remove();
+		});
 	var next_news_feed = newsfeed_stream.shift();
-	$(".newsfeed_block").append("<p>" + next_news_feed + "</p>");
-	setTimeout(updateTicker, random()*4300+1000);
+	$(".newsfeed_block").append("<p>" + next_news_feed + "</p>").find("p:last").hide().slideDown('slow');
+	setTimeout(updateTicker, Math.random()*8300+1000);
 }
